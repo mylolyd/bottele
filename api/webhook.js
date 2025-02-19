6b170220b0791817d9620b9ba71095f012206c50
mylolyd <mylolyd@gmail.com>
2025-02-20 00:55:00 +0700

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json()); // Middleware untuk membaca JSON
+
+app.post('/api/webhook', (req, res) => {
+  console.log('Webhook diterima:', req.body);
+  
+  // Kirim respons ke server pengirim
+  res.status(200).json({ message: 'Webhook diterima' });
+});
+
+module.exports = app;
